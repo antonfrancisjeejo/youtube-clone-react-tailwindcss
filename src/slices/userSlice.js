124150daab8026a5ea4c9275a49ad585ada605d9
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2022-11-29 20:13:23 +0530

    Initial commit

---
+++ src/slices/userSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
+    logout: (state, action) => {
+      state.user = null;
+    },
+  },
+});
+
+export const { setUser, logout } = userSlice.actions;
+
+export const getUser = (state) => state.userInfo.user;
+
+export default userSlice.reducer;
